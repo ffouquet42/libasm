36b98282a37cad842cd8cce22bbe40e95933a4c8
ffouquet42 <ffouquet42@gmail.com>
2026-01-22 09:41:13 +0100

    mac setup and delete comments

--- srcs/asm/ft_strlen.s
+++ srcs/asm/ft_strlen.s
@@ -1,15 +1,15 @@
-global ft_strlen			;
-section .text				;
+global _ft_strlen
+section .text
 
-ft_strlen:					;
-	mov rcx, 0				; copy 0 in rcx			| int i = 0
+_ft_strlen:
+	mov rcx, 0
 
-.loop:						;
-	cmp BYTE [rdi + rcx], 0	; rdi = arg (str)		| if str[0 + i] == '\0'
-	je .end					; jump if equal			| break the loop
-	inc rcx					; increment				| i++
-	jmp .loop				; jump					| loop again
+.loop:
+	cmp BYTE [rdi + rcx], 0
+	je .end
+	inc rcx
+	jmp .loop
 
-.end:						;
-	mov rax, rcx			; rax = returned value	| rax = i
-	ret						; return rax
+.end:
+	mov rax, rcx
+	ret
