cdc44580b05f273de5ec503da444ba6119f50ca8
ffouquet42 <ffouquet42@gmail.com>
2026-01-22 10:27:10 +0100

    ft_strcpy done

--- srcs/asm/ft_strcpy.s
+++ srcs/asm/ft_strcpy.s
@@ -5,4 +5,15 @@ _ft_strcpy:
 	mov rcx, 0
 
 .loop:
-	
+	cmp BYTE [rsi + rcx], 0
+	je .end
+	mov al, BYTE [rsi + rcx]
+	mov BYTE [rdi + rcx], al
+	inc rcx
+	jmp .loop
+
+.end:
+	mov al, [rsi + rcx]
+	mov [rdi + rcx], al
+	mov rax, rdi
+	ret
