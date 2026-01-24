e4834de8d8ba2b5de12808eb2d6ce17ebdcb8992
ffouquet42 <ffouquet42@gmail.com>
2026-01-24 15:03:57 +0100

    ft_write to test

--- srcs/asm/ft_write.s
+++ srcs/asm/ft_write.s
@@ -1,4 +1,22 @@
-global ft_write
+global  ft_write
+extern  __errno_location
+
 section .text
 
 ft_write:
+    mov     rax, 1
+    syscall
+    test    rax, rax
+    jns     .ok
+
+    mov     edi, eax
+    neg     edi
+
+    sub     rsp, 8
+    call    __errno_location wrt ..plt
+    add     rsp, 8
+
+    mov     dword [rax], edi
+    mov     eax, -1
+.ok:
+    ret
