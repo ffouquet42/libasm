e96f507853fe9638228cd6438d19dc0e2e77a79a
ffouquet42 <ffouquet42@gmail.com>
2026-01-27 15:38:54 +0100

    add comments

--- srcs/asm/ft_strcmp.s
+++ srcs/asm/ft_strcmp.s
@@ -1,3 +1,7 @@
+; rax = returned value
+; rdi = const char *s1
+; rsi = const char *s2
+
 global ft_strcmp
 section .text
 
@@ -5,20 +9,28 @@ ft_strcmp:
 	mov rcx, 0
 
 .loop:
-	mov al, BYTE [rdi + rcx]
-	mov dl, BYTE [rsi + rcx]
+	mov al, [rdi + rcx]
+	mov dl, [rsi + rcx]
 
-	cmp al, dl
-	jne .end
+	cmp al, dl				; if al != dl -> ZF = 0
+	jne .end				; if ZF == 0, jump to end
 
-	test al, al
-	je .end
+	test al, al				; check if al == 0 (null terminator) -> ZF = 1
+	je .end					; if ZF == 1, jump to end
 
 	inc rcx
 	jmp .loop
 
 .end:
-	movzx eax, al
+	movzx eax, al			 
 	movzx edx, dl
 	sub eax, edx
 	ret
+
+; section .note.GNU-stack noalloc noexec
+
+; MOve with Zero eXtend / copy little value to bigger register, filling high bits with zeros (left side)
+; al, dl 8 bits
+; eax, edx 32 bits
+; strcmp return integer: int
+; int don't fit in 8 bits (char), need at least 32 bits (int)
--- srcs/asm/ft_strlen.s
+++ srcs/asm/ft_strlen.s
@@ -4,17 +4,20 @@
 global ft_strlen			; Make function globally accessible
 section .text				; Declare text (code) section
 
-ft_strlen:					; Function entry point
+ft_strlen:
 	mov rcx, 0				; Initialize counter
 
-.loop:						; Start of loop
+.loop:
 	cmp BYTE [rdi + rcx], 0	; Check for null terminator
 	je .end					; If found, exit loop
 	inc rcx					; Increment counter	
-	jmp .loop				; Repeat
+	jmp .loop
 
 .end:
 	mov rax, rcx			; Move counter to rax
-	ret						; Return
+	ret						; Return rax
 
 ; section .note.GNU-stack noalloc noexec
+
+; cmp update CPU flags (zero flag, sign flag, etc.)
+; je (jump if equal) checks the zero flag (jump if ZF == 1)
