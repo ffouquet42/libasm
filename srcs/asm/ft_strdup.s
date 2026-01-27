d27bbc3665565f38c79f82c8231877fbe66cc414
ffouquet42 <ffouquet42@gmail.com>
2026-01-27 17:42:22 +0100

    add comments

--- srcs/asm/ft_strdup.s
+++ srcs/asm/ft_strdup.s
@@ -1,4 +1,8 @@
+; rax = returned value
+; rdi = const char *str
+
 global  ft_strdup
+
 extern  ft_strlen
 extern  malloc
 extern  ft_strcpy
@@ -6,24 +10,30 @@ extern  ft_strcpy
 section .text
 
 ft_strdup:
-    push    rbx
-    sub     rsp, 8
-    mov     rbx, rdi
-    mov     rdi, rbx
-    call    ft_strlen
-    inc     rax
-    mov     rdi, rax
-    call    malloc wrt ..plt
-    test    rax, rax
-    je      .fail
-    mov     rdi, rax
-    mov     rsi, rbx
-    call    ft_strcpy
-    add     rsp, 8
-    pop     rbx
-    ret
+    push rbx					; non-volatile register, save original value on stack
+    mov rbx, rdi				; stock str in rbx (non-volatile register) because rdi will be erased by calls
 
-.fail:
-    add     rsp, 8
-    pop     rbx
-    ret
+    call ft_strlen				; rax = strlen(str)
+    inc rax						; + 1 for null byte
+
+    mov rdi, rax				; put size to malloc in rdi (first argument)
+    call malloc wrt ..plt		; rax = ptr if malloc success, 0 if fail
+
+    test rax, rax				; check if malloc fail (rax == 0) (ZF = 1)
+    je .malloc_fail				; jump if malloc fail (ZF == 1)
+
+    mov rdi, rax				; setup dest for strcpy (malloc ptr)
+    mov rsi, rbx				; setup src for strcpy (original str)
+    call ft_strcpy				; rax = dest (malloc ptr)
+
+    pop rbx						; restore original value of rbx
+    ret							; return malloc ptr
+
+.malloc_fail:
+    pop rbx						; restore original value of rbx
+    ret							; return 0
+
+; section .note.GNU-stack noalloc noexec
+
+; This function makes several calls I need a non-volatile register to keep a value across calls (rbx)
+; I cannot know if it is already used, I save original value on the stack, use it as a local variable, then restore it
